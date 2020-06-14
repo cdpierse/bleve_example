6ad96d89ef0978bbcd89e5e6e804feb4ddef6178
Charles Pierse <charlespierse@gmail.com>
2020-06-14 01:04:11 +0100

    Refactoring

--- main.go
+++ main.go
@@ -14,19 +14,25 @@ func main() {
 
 	flag.Parse()
 	log.Println(*dataset)
-	articles, err := ReadDataset(*dataset)
-	if err != nil {
-		log.Print(err)
-	}
+	// articles, err := ReadDataset(*dataset)
+	// if err != nil {
+	// 	log.Print(err)
+	// }
 
-	index, err := GetIndex("example1.bleve")
-	if err != nil {
-		log.Print(err)
-	}
-	log.Println("Loaded Index")
+	// index, err := GetIndex("example1.bleve")
+	// if err != nil {
+	// 	log.Print(err)
+	// }
+	// log.Println("Loaded Index")
 
-	matches, err := MatchQueryIndex("California fires rage on",index)
-	GetQueryHits(matches,articles)
-
+	// longstring := `
+	// A combination of dry weather and record heat in recent days has led to several wildfires in California, forcing some evacuations on Wednesday.
+	// Officials in Ventura County issued an evacuation order for a remote area of Piru located about 48 miles northwest of downtown Los Angeles for a blaze known as the Lime Fire.
+	// The blaze broke out Wednesday afternoon near the Lake Piru campground and has burned about 450 acres so far. 
+	// Fire officials said Thursday that two minor injuries have been reported so far, and the fire is 20 percent contained.
+	// `
+	// matches, err := MatchQueryIndex(longstring, index)
+	// GetQueryHits(matches, articles)
+	GetTemplate()
 
 }
---
+++ template.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"html/template"
+	"log"
+)
+
+// func GetTemplate(){
+// 	t, err := template.ParseFiles("index.html")
+// 	log.Println(t.,err)
+// }
