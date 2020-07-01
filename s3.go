ecabd05c365e645c4b83af3ea423b73beb289670
Charles Pierse <charlespierse@gmail.com>
2020-07-01 22:13:55 +0100

    Add upload test

--- s3.go
+++ s3.go
@@ -6,12 +6,10 @@ import (
 	_ "log"
 	"os"
 	"path/filepath"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-	"github.com/blevesearch/bleve"
 )
 
 // Change these depending on your bucket, and region
@@ -20,37 +18,26 @@ const (
 	S3BUCKET = "bleve-search-example"
 )
 
-// UploadIndexDir uploads the given bleve index an S3 bucket
-func UploadIndexDir(index *bleve.Index, name string) error {
-	return nil
-}
+// func main() {
+// 	sess, err := session.NewSession(&aws.Config{
+// 		Region: aws.String(S3REGION)},
+// 	)
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-// DownloadIndexDir downloads the latest saved version of an index
-// from S3
-func DownloadIndexDir(name string) error {
-	return nil
-}
+// 	if ok, err := BucketExists(sess, S3BUCKET); !ok {
+// 		panic(err)
+// 	}
 
-func main() {
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String(S3REGION)},
-	)
-	if err != nil {
-		panic(err)
-	}
+// 	// UploadIndex("example1.bleve")
+// 	resp, err := ObjectExists(sess, "index_mta.json", S3BUCKET)
+// 	if err != nil {
+// 		log.Panic(err)
+// 	}
+// 	log.Println(resp)
 
-	if ok, err := CheckBucketExists(sess, S3BUCKET); !ok {
-		panic(err)
-	}
-
-	// UploadIndex("example1.bleve")
-	resp, err := CheckBucketObjectExists(sess, "index_mta.json", S3BUCKET)
-	if err != nil {
-		log.Panic(err)
-	}
-	log.Println(resp)
-
-}
+// }
 
 // NewSession creates a new seission service client.
 func NewSession() (*session.Session, error) {
@@ -64,14 +51,14 @@ func NewSession() (*session.Session, error) {
 
 }
 
-// CheckBucketExists checks if a bucket exists for given aws account
+// BucketExists checks if a bucket exists for given aws account
 //	Inputs:
 //     sess: is the current session, which provides configuration for the SDK's service clients
 //     name: is the name of the bucket
 //	Output:
 //     true if the bucket exists, false if not
 //     error will not be nil if something goes wrong
-func CheckBucketExists(sess *session.Session, name string) (bool, error) {
+func BucketExists(sess *session.Session, name string) (bool, error) {
 	client := s3.New(sess)
 	result, err := client.ListBuckets(nil)
 	if err != nil {
@@ -85,7 +72,15 @@ func CheckBucketExists(sess *session.Session, name string) (bool, error) {
 	return false, errors.New("s3 bucket '" + name + " ' does not exist.")
 }
 
-func CheckBucketObjectExists(sess *session.Session, filename string, bucket string) (bool, error) {
+// ObjectExists asserts whether a file(object) exists in a given bucket.
+//	Inputs:
+//     sess: is the current session, which provides configuration for the SDK's service clients
+//     filename: is the file(object) we are testing if it exists
+//	   bucket: is the S3 bucket the filename for in
+//	Output:
+//     true: if the file exists, false if not
+//     error will not be nil if something goes wrong
+func ObjectExists(sess *session.Session, filename string, bucket string) (bool, error) {
 	client := s3.New(sess)
 	response, err := client.ListObjectsV2(&s3.ListObjectsV2Input{
 		Bucket: aws.String(bucket),
@@ -123,19 +118,21 @@ func FileUpload(sess *session.Session, fp string) error {
 }
 
 // UploadIndex finds all files in a given directory and individually adds each
-// to the bleve index bucket.
-func UploadIndex(root string) error {
+// one at a time to the bleve index bucket.
+func UploadIndex(sess *session.Session, root string) error {
 	var files []string
+
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
 			files = append(files, path)
 		}
 		return nil
 	})
+
 	if err != nil {
 		return err
 	}
-	sess, err := NewSession()
+
 	if err != nil {
 		panic(err)
 	}
--- s3_test.go
+++ s3_test.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	_ "log"
-	_"os"
+	_ "os"
 	"testing"
 )
 
@@ -36,8 +36,18 @@ const s3TestIndex = "s3Test.bleve"
 // 	}
 // }
 
-
 func TestUpload(t *testing.T) {
+	filename := "test.txt"
+	sess, err := NewSession()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = FileUpload(sess, filename)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-
+	if ok, err := ObjectExists(sess, filename, S3BUCKET); !ok {
+		t.Fatal(err)
+	}
 }
